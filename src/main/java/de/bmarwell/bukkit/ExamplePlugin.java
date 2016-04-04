8594b0245b23f42a2b61b20868f389e6b5b5521f
Benjamin Marwell <bmarwell@gmail.com>
2016-04-04 21:37:11 +0200

    Very basic structure.

---
+++ src/main/java/de/bmarwell/bukkit/ExamplePlugin.java
@@ -0,0 +1,31 @@
+/**
+ * Bukkit-Plugin-Example by Benjamin Marwell.
+ *
+ * To the extent possible under law, the person who associated CC0 with
+ * Bukkit-Plugin-Example has waived all copyright and related or neighboring rights
+ * to Bukkit-Plugin-Example.
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work.  If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
+package de.bmarwell.bukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * The example plugin main class.
+ *
+ * @since 0.0.1-SNAPSHOT.
+ */
+public class ExamplePlugin extends JavaPlugin {
+
+  @Override
+  public void onDisable() {
+  }
+
+  @Override
+  public void onEnable() {
+  }
+
+}
---
+++ src/main/java/de/bmarwell/bukkit/events/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * Bukkit-Plugin-Example by Benjamin Marwell.
+ *
+ * To the extent possible under law, the person who associated CC0 with
+ * Bukkit-Plugin-Example has waived all copyright and related or neighboring rights
+ * to Bukkit-Plugin-Example.
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work.  If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
+/**
+ * Custom events go here.
+ *
+ */
+package de.bmarwell.bukkit.events;
